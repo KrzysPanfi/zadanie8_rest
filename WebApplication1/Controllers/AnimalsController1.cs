aaeb1bc08d50ed05db7623a3193d4b879fc04375
Krzysztof <krzyspanfilewicz@gmail.com>
2024-11-22 13:36:08 +0100

    Animals

---
+++ WebApplication1/Controllers/AnimalsController1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        public List<Animal> database = new List<Animal>();
+        public AnimalsController()
+        {
+            Animal a1 = new Animal { Id = 1, Name = "Rex", category = "pies", color = "brown", mass = 12 };
+        }
+        [HttpGet]
+        public IActionResult GetAnimlas()
+        {
+            return Ok(database);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAnimal(int id)
+        {
+            return Ok("bbb");
+        }
+    }
+}
---
+++ WebApplication1/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string category { get; set; }
+
+        public int mass { get; set; }
+        public string color { get; set; }
+    }
+}
